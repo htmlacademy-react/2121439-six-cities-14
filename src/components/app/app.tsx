63d6224a42315d87f14546609266269d665117fa
Egor <egorviliavin@gmail.com>
2023-10-14 22:55:58 +0300

    Созданы новые страницы в папке src/pages, созданы компоненты App, City Card в папке src/components. Так же прописан props(CountPlaces)-колличество предложений аренды на странице six-cities-screen-page.tsx, и все это выведено в компонент App.tsx. Линт ошибок не дал.

---
+++ src/components/app/app.tsx
@@ -0,0 +1,7 @@
+import SixCitiesScreen from '../../pages/six-cities-main-page/six-citites-screen-page';
+
+function App(): JSX.Element {
+  return <SixCitiesScreen countOffer={116} />;
+}
+
+export default App;
