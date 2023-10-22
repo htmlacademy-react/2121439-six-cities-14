30a667b5482f50c96c8e5ecf7113f8fc55dd23e0
Egor <egorviliavin@gmail.com>
2023-10-22 14:44:01 +0300

    Создал маршрут для страницы favorites в соотвествии с тз

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,18 +1,32 @@
-import SixCitiesScreen from '../../pages/six-cities-main-page/six-citites-screen-page';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //Импорт pages для роутера
+import SixCitiesScreen from '../../pages/six-cities-main-page/six-citites-screen-page';
 import LoginScreenPage from '../../pages/login-screen-page/login-screen-page';
 import OfferScreenPage from '../../pages/offer-screen-page/offer-screen-page';
 import FavoritesScreenPage from '../../pages/favorites-screen-page/favorites-screen-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import PrivateRoute from '../private-route/private-route';
+
+import { AppRoute, AuthorizationStatus } from '../../const';
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SixCitiesScreen countOffer={200} />} />
-        <Route path="/login" element={<LoginScreenPage />} />
-        <Route path="/offer/:id" element={<OfferScreenPage />} />
-        <Route path="/favorites" element={<FavoritesScreenPage />} />
+        <Route
+          path={AppRoute.Main}
+          element={<SixCitiesScreen countOffer={200} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesScreenPage />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoute.Login} element={<LoginScreenPage />} />
+        <Route path={AppRoute.Offer} element={<OfferScreenPage />} />
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export enum AppRout {
+export enum AppRoute {
   Main = '/',
   Login = '/login',
   Favorites = '/favorites',
